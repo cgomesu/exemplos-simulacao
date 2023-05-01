fc472be71bec5b9f72eb2068e356e365badc1874
cgomesu <cf365@cornell.edu>
2023-05-01 16:29:05 -0300

    fix: shebang

--- __main__.py
+++ __main__.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
 from simulacao import Binomial
 
 def main():
     m = 1000
+    seed = 6548
     n = 10
     theta = .5
-    binomial = Binomial(m, n, theta)
+    binomial = Binomial(m, seed, n, theta)
     binomial.simulacao(234)
     binomial.histograma()
 
